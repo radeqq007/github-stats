2bc90aae51f81d2652048ebdc9197e9d3d45ab6e
radeqq007 <radeqq007@interia.pl>
2025-01-28 14:43:17 +0100

    feat: hero section responsivness

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center gap-40">
-    <h1 class="font-black text-8xl">YOUR GITHUB STATS</h1>
+  <div class="h-screen flex flex-col justify-center items-center gap-10 md:gap-40">
+    <h1 class="font-black text-6xl text-center md:text-8xl">YOUR GITHUB STATS</h1>
     <span class="flex flex-col items-center gap-8">
-      <input type="text" placeholder="Your username..." v-model="username" class="w-100 border-2 border-purple-600 rounded-3xl p-3 text-gray-100 text-center text-lg font-light bg-gray-900"/>
-      <button @click="updateUsername" class="w-100 bg-purple-600 rounded-3xl p-3 ">GET STATS</button>
+      <input type="text" placeholder="Your username..." v-model="username" class="w-70 md:w-100 border-2 border-purple-600 rounded-3xl p-1 md:p-3 text-gray-100 text-center text-lg font-light bg-gray-900"/>
+      <button @click="updateUsername" class="w-70 md:w-100 bg-purple-600 rounded-3xl p-2 md:p-3 ">GET STATS</button>
     </span>
   </div>
 </template>
