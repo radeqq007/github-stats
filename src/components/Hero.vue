f3a86aabd218a6e79c8837f9bbb1613ea0dba684
radeqq007 <radeqq007@interia.pl>
2025-01-28 15:55:29 +0100

    style: change header styling

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center gap-10 md:gap-40">
-    <h1 class="font-black text-6xl text-center md:text-8xl">YOUR GITHUB STATS</h1>
+    <h1 class="font-black text-6xl text-center md:text-9xl w-160">YOUR GITHUB STATS</h1>
     <span class="flex flex-col items-center gap-8">
       <input type="text" placeholder="Your username..." v-model="username" class="w-70 md:w-100 border-2 border-purple-600 rounded-3xl p-1 md:p-3 text-gray-100 text-center text-lg font-light bg-gray-900"/>
       <button @click="updateUsername" class="w-70 md:w-100 bg-purple-600 rounded-3xl p-2 md:p-3 ">GET STATS</button>
