3244efaf20722956f5e3b84f926c7693cc8901ac
radeqq007 <radeqq007@interia.pl>
2025-01-30 11:16:45 +0100

    style: change card appearance

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,12 +2,14 @@
   <div class="w-11/12 m-auto" id="dashboard">
     <h2 v-if="!username">Enter your github username.</h2>
     <div class="grid grid-cols-2 gap-4">
-      <div class="w-full col-span-2 p-10 bg-zinc-800 rounded-lg">
+      <div
+        class="w-full col-span-2 p-10 bg-zinc-800/60 backdrop-blur-xs rounded-lg"
+      >
         <h3 v-if="!username">Profile</h3>
         <UserProfile class="" :username="username" />
       </div>
       <div
-        class="bg-zinc-800 rounded-lg px-10 py-5 flex flex-col gap-10 col-span-2 md:col-span-1"
+        class="bg-zinc-800/60 backdrop-blur-xs rounded-lg px-10 py-5 flex flex-col gap-10 col-span-2 md:col-span-1"
       >
         <h3 class="font-bold text-4xl text-center">Most used languages:</h3>
 
@@ -16,7 +18,7 @@
         </div>
       </div>
       <div
-        class="bg-zinc-800 rounded-lg px-10 py-5 flex flex-col gap-10 col-span-2 md:col-span-1"
+        class="bg-zinc-800/60 backdrop-blur-xs rounded-lg px-10 py-5 flex flex-col gap-10 col-span-2 md:col-span-1"
       >
         <h3 class="font-bold text-4xl text-center">Types of repositories:</h3>
 
