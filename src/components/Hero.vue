002ca41fdc3ed08798ce73aa074c3d300163c2d8
radeqq007 <radeqq007@interia.pl>
2025-01-30 13:12:44 +0100

    style: change input appearance

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,9 +1,33 @@
 <template>
-  <div class="h-screen flex flex-col justify-center items-center gap-10 md:gap-40">
-    <h1 class="font-black text-7xl text-center md:text-9xl w-full md:w-160">YOUR GITHUB STATS</h1>
+  <div
+    class="h-screen flex flex-col justify-center items-center gap-10 md:gap-40"
+  >
+    <h1 class="font-black text-7xl text-center md:text-9xl w-full md:w-160">
+      YOUR GITHUB STATS
+    </h1>
     <span class="flex flex-col items-center gap-8">
-      <input type="text" placeholder="Your username..." v-model="username" class="w-70 md:w-100 border-2 border-purple-600 rounded-3xl p-1 md:p-3 text-gray-100 text-center text-lg font-light bg-zinc-900"/>
-      <button @click="updateUsername" class="w-70 md:w-100 bg-purple-600 rounded-3xl p-2 md:p-3 ">GET STATS</button>
+      <span
+        class="relative bg-linear-to-r from-purple-600 to-blue-600 rounded-3xl"
+      >
+        <span
+          class="absolute inset-0.5 bg-zinc-900 group-hover:opacity-0 duration-100 rounded-3xl"
+        ></span>
+        <input
+          type="text"
+          placeholder="Your username..."
+          v-model="username"
+          class="relative z-10 w-70 md:w-100 focus:outline-0 rounded-3xl p-1 md:p-3 text-gray-100 text-center text-lg font-light"
+        />
+      </span>
+      <button
+        @click="updateUsername"
+        class="group relative w-70 md:w-100 bg-linear-to-r from-purple-600 to-blue-600 rounded-3xl p-2 md:p-3"
+      >
+        <span
+          class="absolute inset-0.5 bg-zinc-900 opacity-0 md:opacity-100 group-hover:opacity-0 duration-100 rounded-3xl"
+        ></span>
+        <span class="relative z-10"> GET STATS </span>
+      </button>
     </span>
   </div>
 </template>
@@ -26,5 +50,3 @@ const updateUsername = () => {
   dashboardElement.scrollIntoView({ behavior: 'smooth' });
 };
 </script>
-
-
